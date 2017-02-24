6390b335b147f3afcc1ad92e076a58ef4f5ce858
Stephanie Miller <smillers@umich.edu>
2017-02-24 12:43:53 -0500

    Practice adding multiple entries (1 table) at once

--- lecture8_sql.py
+++ lecture8_sql.py
@@ -16,4 +16,18 @@ statement = 'DELETE FROM Tracks'
 cur.execute(statement)
 conn.commit()
 
+data_items = [
+	('London Calling', 'The Clash', 'London Calling', 235),
+	('Anarchy in the UK', 'The Sex Pistols', 'Nevermind the Bollocks', 144),
+	('Blitzkrieg Bop', 'The Ramones', 'The Ramones', 89)
+]
+
+statement = 'INSERT INTO Tracks VALUES (?, ?, ?, ?)'
+
+for item in data_items:
+	cur.execute(statement, item)
+conn.commit()
+
+#Research impact of this method on potential SQL injection attacks
+
 conn.close()
